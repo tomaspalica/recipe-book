bb7ee73426d1e03e14f3fc77997aadf2d4e7e12f
tomaspalica <tomasss.palica@gmail.com>
2024-03-04 22:28:15 +0100

    Recipe Details change
    im working on RecipeDetails page to show ingredients steps and other informaction about the recipe

--- src/apiFetch/Api.js
+++ src/apiFetch/Api.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 
 const key = process.env.REACT_APP_API_KEY;
-axios.defaults.baseURL = "https://api.spoonacular.com";
+const edamamKey = process.env.REACT_APP_EDAMAM_KEY;
+const edamamID = process.env.REACT_APP_EDAMAM_ID;
+// axios.defaults.baseURL = "https://api.spoonacular.com";
 export const fetchRecipes = async (meal) => {
   console.log(process.env);
   const respone = await axios.get(
-    `/recipes/complexSearch?query=${meal}&apiKey=${key}`
+    `https://api.spoonacular.com/recipes/complexSearch?query=${meal}&apiKey=${key}`
   );
   return respone;
 };
 
 export const fetchRecipesDetails = async (id) => {
-  console.log(typeof id);
   const respone = await axios.get(
     `http://api.spoonacular.com/recipes/${id}/information?apiKey=${key}`
   );
--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -4,15 +4,21 @@ import { fetchRecipesDetails } from "../apiFetch/Api";
 
 export const RecipeDetails = () => {
   const [recipe, setRecipe] = useState();
+  const [ingredients, setIngredients] = useState();
   const { id } = useParams();
-
+  let ourIngredients = [];
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetchRecipesDetails(id);
         console.log(response);
         setRecipe(response.data);
-        console.log(recipe.analyzedInstructions);
+        const filteredIngredients = recipe?.analyzedInstructions.map((el) =>
+          el.steps.map((el) => el.ingredients.map((el) => el.name))
+        );
+        console.log(filteredIngredients);
+        setIngredients(ourIngredients);
+        console.log(ingredients);
       } catch (error) {
         console.log(error);
       }
@@ -23,10 +29,18 @@ export const RecipeDetails = () => {
   return (
     <main>
       <h2>{recipe?.title}</h2>
-      <ul>
-        <li></li>
-      </ul>
       <ul></ul>
+
+      {recipe?.analyzedInstructions?.map((el) => (
+        <ul>
+          <h3>{el.name}</h3>{" "}
+          {el.steps.map((step) => (
+            <li key={step.number}>
+              {step.number}: {step.step}
+            </li>
+          ))}{" "}
+        </ul>
+      ))}
     </main>
   );
 };
