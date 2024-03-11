ab52aefdbecbcf3236fa732e3cdd4661000dcbc8
tomaspalica <tomasss.palica@gmail.com>
2024-03-11 22:36:01 +0100

    fixed ingredients issue
    i have fixed the issue where ingredients woulndt show up or show up with duplicates from api request

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchRecipesDetails } from "../apiFetch/Api";
-
+import { nanoid } from "nanoid";
 export const RecipeDetails = () => {
   const [recipe, setRecipe] = useState();
   const [ingredients, setIngredients] = useState();
@@ -13,12 +13,16 @@ export const RecipeDetails = () => {
         const response = await fetchRecipesDetails(id);
         console.log(response);
         setRecipe(response.data);
-        const filteredIngredients = recipe?.analyzedInstructions.map((el) =>
-          el.steps.map((el) => el.ingredients.map((el) => el.name))
+        const filteredIngredients = await recipe?.extendedIngredients.filter(
+          (obj, index) => {
+            return (
+              recipe?.extendedIngredients.findIndex(
+                (item) => item.id === obj.id
+              ) === index
+            );
+          }
         );
-        console.log(filteredIngredients);
-        setIngredients(ourIngredients);
-        console.log(ingredients);
+        setIngredients(filteredIngredients);
       } catch (error) {
         console.log(error);
       }
@@ -29,13 +33,16 @@ export const RecipeDetails = () => {
   return (
     <main>
       <h2>{recipe?.title}</h2>
-      <ul></ul>
-
+      <ul>
+        {ingredients?.map((el) => (
+          <li key={el.id}>{el.original}</li>
+        ))}
+      </ul>
       {recipe?.analyzedInstructions?.map((el) => (
-        <ul>
+        <ul key={nanoid()}>
           <h3>{el.name}</h3>{" "}
           {el.steps.map((step) => (
-            <li key={step.number}>
+            <li key={nanoid()}>
               {step.number}: {step.step}
             </li>
           ))}{" "}
