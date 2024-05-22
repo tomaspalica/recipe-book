c6bbcd2ae7e927eec91c121029a49f3f4163acbf
tomaspalica <tomasss.palica@gmail.com>
2024-05-22 12:09:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/recipebook">
+    <BrowserRouter basename="/recipe-book">
       <App />
     </BrowserRouter>
   </React.StrictMode>
