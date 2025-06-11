30eb3bae0a3b5bfbc22d4fb4403482c2e311cf79
tomaspalica <tomasss.palica@gmail.com>
2025-06-11 19:41:33 +0200

    quick fix

--- api/moderateComment.js
+++ api/moderateComment.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         model: "gpt-3.5-turbo",
-        message: [
+        messages: [
           {
             role: "system",
             content:
