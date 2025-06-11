d8325b35ec20f5b67376ecf153b89523c3b3f689
tomaspalica <tomasss.palica@gmail.com>
2025-06-11 18:18:07 +0200

    vercel fix 3

--- src/index.js
+++ src/index.js
@@ -7,11 +7,10 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/recipe-book">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
 
 reportWebVitals();
